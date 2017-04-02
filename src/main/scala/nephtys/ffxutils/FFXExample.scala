8719ffb5493c64f5f4a463f0187646cf76665c5b
Nephtys <christopher.kaag@gmail.com>
2017-04-02 14:28:19 +0200

    basic version implemented

--- src/main/scala/nephtys/ffxutils/FFXExample.scala
+++ src/main/scala/nephtys/ffxutils/FFXExample.scala
@@ -6,23 +6,33 @@ import javafx.scene.control.Button
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
+import scala.concurrent.Future
+
 /**
   * Created by Christopher on 02.04.2017.
   */
 class FFXExample extends javafx.application.Application{
+  val POPUPMENU_ID = "DEFAULT_POPUPMENU"
+
   override def start(primaryStage: Stage): Unit = {
     primaryStage.setTitle("Hello World!")
     val btn = new Button
     btn.setText("Say 'Hello World'")
+
+    val popuppane = new PopupMenuPane(POPUPMENU_ID, 150, 200, btn)
+    popuppane.setupActiveHandler((a : Object, b : Object) => println(s"$a pressed $b"))
+    popuppane.setupPassiveHandlers(Array(("First button", obj => println(s"Pressed on first button with object $obj")), ("Second button", obj => println(s"Pressed on second button with object $obj"))))
+
+
     btn.setOnAction(new EventHandler[ActionEvent] {
       override def handle(e: ActionEvent) {
         println("Hello World!")
+        popuppane.showPopupThreadsafe(btn.localToScene(btn.getBoundsInLocal).getMinX, btn.localToScene(btn.getBoundsInLocal).getMinY, Future.successful(Array.empty[(String, Object)]), "Some title", "this is an object" )
       }
     })
 
-    val root = new StackPane
-    root.getChildren.add(btn)
-    primaryStage.setScene(new Scene(root, 800, 600))
+
+    primaryStage.setScene(new Scene(popuppane, 800, 600))
     primaryStage.show()
   }
 }
--- src/main/scala/nephtys/ffxutils/PopupMenuPane.scala
+++ src/main/scala/nephtys/ffxutils/PopupMenuPane.scala
@@ -2,12 +2,15 @@ package nephtys.ffxutils
 
 import java.lang.ref.WeakReference
 import java.util
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.Callable
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
+import java.util.function.Consumer
 import javafx.application.Platform
 import javafx.event.{ActionEvent, EventHandler}
 import javafx.scene.Node
 import javafx.scene.control.{Button, Label}
-import javafx.scene.layout.{BorderPane, Pane, VBox}
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout._
 
 import scala.concurrent.Future
 
@@ -15,23 +18,28 @@ import scala.concurrent.Future
 /**
   * Created by Christopher on 02.04.2017.
   */
-class PopupMenuPane(background : Node, id : String, popupWidth : Double, popupHeight : Double, columns : Int = 2, maxActiveButtonNumber : Int = 4) extends Pane {
+class PopupMenuPane(id: String, popupWidth: Double, popupHeight: Double, background: Node, columns: Int = 2, maxActiveButtonNumber: Int = 4) extends StackPane {
+
+
+  this.setStyle("-fx-background-color: #AA0000;")
 
   //contains invisible menu that can be shown and rerendered via methods and another background
 
-  //TODO: add frontpane with absolute positioning for borderpane
-  //TODO: make PopupMenuPane a Stackpane and stretch background Node to full parent in background
-
-
-  private val _buttons : util.ArrayList[Button] = {
+  private val _buttons: util.ArrayList[Button] = {
     var list = new util.ArrayList[Button]()
-    (1 to maxActiveButtonNumber).foreach(i => {
+    (0 until maxActiveButtonNumber).foreach(i => {
       val btn = new Button()
       btn.setText("N/A")
       btn.setVisible(false)
       btn.setOnAction(new EventHandler[ActionEvent] {
         override def handle(event: ActionEvent): Unit = {
-
+          val first_object = _firstObject.get()
+          val x = activeObjects.get()
+          if (first_object != null && x.length > i) {
+            activeHandler.apply(first_object, x.apply(i))
+          } else {
+            println("Error in active button eventhandler")
+          }
         }
       })
       list.add(btn)
@@ -40,72 +48,184 @@ class PopupMenuPane(background : Node, id : String, popupWidth : Double, popupHe
   }
 
 
-  private val _activeObjects : Array[Object] = new Array[Object](maxActiveButtonNumber)
+  protected def clipAndSetCoordinates(x: Double, y: Double): Unit = {
+    //get total width height
+    val w = foreground.getWidth
+    val h = foreground.getHeight
+
+    //clip against each of the 4 sides once by moving to the center
+    val left: Double = Math.min(Math.max(0, x), w - popupWidth)
+    val above: Double = Math.min(Math.max(0, y), h - popupHeight)
+
+    //set constraints to match
+    gridconstraint_left.setPrefWidth(left)
+    gridconstraint_above.setPrefHeight(above)
+  }
+
 
   protected val popup = new BorderPane()
+  popup.setStyle("-fx-background-color: #000000;")
   protected val activeButtons = new VBox()
   activeButtons.getChildren.setAll(_buttons)
   protected val passiveButtons = new VBox()
-  protected val titleLabel = new Label {}
+  protected val titleLabel = new Label()
+  popup.setTop(titleLabel)
+  popup.setCenter(activeButtons)
+  popup.setBottom(passiveButtons)
+
   titleLabel.getStyleClass.addAll("base-padding", "quickmenu-colors")
   titleLabel.setStyle("-fx-alignment: center")
 
+
+  private val activeObjects: AtomicReference[Array[(String, Object)]] = new AtomicReference(Array.empty)
+
+
+  //add frontpane with absolute positioning for borderpane
+  protected val foreground: GridPane = new GridPane() //used for pseudo-absolute positioning, using a 9 cell grid whose constraints are moved from time to time
+  foreground.setVisible(false)
+  private val gridconstraint_left: ColumnConstraints = new ColumnConstraints(0)
+  private val gridconstraint_above: RowConstraints = new RowConstraints(0)
+  private val gridconstraint_central_column: ColumnConstraints = new ColumnConstraints(popupWidth)
+  private val gridconstraint_central_row: RowConstraints = new RowConstraints(popupHeight)
+  foreground.getColumnConstraints.add(gridconstraint_left)
+  foreground.getColumnConstraints.add(gridconstraint_central_column)
+  foreground.getRowConstraints.add(gridconstraint_above)
+  foreground.getRowConstraints.add(gridconstraint_central_row)
+  GridPane.setConstraints(popup, 1, 1)
+  foreground.getChildren.add(popup)
+  this.getChildren.addAll(background, foreground)
+  foreground.setStyle("-fx-background-color: #00FF00;")
+
+  //add foreground action handlers to close popup on any click that is not on the popup
+  foreground.setOnMouseClicked(new EventHandler[MouseEvent] {
+    override def handle(event: MouseEvent): Unit = {
+      closeInThread()
+    }
+  })
+
+
   //second type of Objects
-  def showPopupThreadsafe(positionX : Double, positionY : Double, buttons : Future[Array[(String, Object)]], title : String): Unit = {
-    ???
+  def showPopupThreadsafe(positionX: Double, positionY: Double, buttons: Future[Array[(String, Object)]], title: String, firstObjectType: Object): Unit = {
+    println(s"Showing popup at position $positionX and $positionY")
+    _firstObject.set(firstObjectType) //set user
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Platform.runLater(new Runnable {
+      //first: make every active button invisible and set title
+      override def run(): Unit = {
+
+        titleLabel.setText(title)
+
+        _buttons.forEach(new Consumer[Button] {
+          override def accept(t: Button): Unit = t.setVisible(false)
+        })
+      }
+    })
+    buttons.foreach(a => {
+      activeObjects.set(a)
+      Platform.runLater(new Runnable {
+        override def run(): Unit = {
+          //afterwards, make every active button visible that is needed
+          require(a.length <= _buttons.size())
+          a.indices.foreach(i => {
+            //activate button
+            _buttons.get(i).setText(a.apply(i)._1)
+            _buttons.get(i).setVisible(true)
+          })
+          foreground.setVisible(true)
+        }
+      })
+    })
+    //clipping
+    clipAndSetCoordinates(positionX, positionY)
+    //showing
+    foreground.setVisible(true)
+
   }
 
-  def isShown : Boolean = this.popup.isVisible
+  private val _firstObject: AtomicReference[Object] = new AtomicReference[Object](null)
 
-  def closeThreadsafe() : Unit = {
-    ???
+  def isShown: Boolean = this.foreground.isVisible
+
+  def closeInThread(): Unit = {
+    foreground.setVisible(false)
+    _firstObject.set(null)
   }
 
-  def setupActiveHandler(handler : (Object, Object) => Unit ) : Unit = {
+  def closeThreadsafe(): Unit = {
+    Platform.runLater(new Runnable {
+      override def run(): Unit = {
+        closeInThread()
+      }
+    })
+  }
+
+  def setupActiveHandler(handler: (Object, Object) => Unit): Unit = {
     activeHandler = handler
   }
 
-  def setupPassiveHandlers(handlers : Array[(String, Object => Unit)] ) : Unit = {
+  def setupPassiveHandlers(handlers: Array[(String, Object => Unit)]): Unit = {
     //passiveHandlers = handlers
     Platform.runLater(new Runnable {
       override def run(): Unit = {
+        val _buttons = new util.ArrayList[Button]()
         handlers.foreach(a => {
-
+          val b = new Button() //passive buttons are declared here
+          b.setText(a._1)
+          b.setOnAction(new EventHandler[ActionEvent] {
+            override def handle(event: ActionEvent): Unit = if (_firstObject.get() != null) {
+              a._2.apply(_firstObject.get())
+            }
+          })
+          _buttons.add(b)
+        })
+        Platform.runLater(new Runnable {
+          override def run(): Unit = passiveButtons.getChildren.setAll(_buttons)
         })
       }
     })
 
   }
 
-  protected def setTitle(str : String) : Unit = this.titleLabel.setText(str)
+  protected def setTitle(str: String): Unit = this.titleLabel.setText(str)
 
 
-  private var activeHandler : (Object, Object) => Unit = {
-    (a: Object,  b: Object) => println("ActiveHandler in PopupMenuPane not set")
+  private var activeHandler: (Object, Object) => Unit = {
+    (a: Object, b: Object) => println("ActiveHandler in PopupMenuPane not set")
   }
   //not needed: private var passiveHandlers : Array[(String, Object => Unit)] = Array.empty
 
 }
+
 //all drinks, give out, statistics
 
-object PopupMenuPane{
-  private val _panes : util.HashMap[String, WeakReference[PopupMenuPane]] = new util.HashMap[String, WeakReference[PopupMenuPane]]()
+object PopupMenuPane {
+  private val _panes: util.HashMap[String, WeakReference[PopupMenuPane]] = new util.HashMap[String, WeakReference[PopupMenuPane]]()
 
-  def addWeakReference(popupId : String, pane : PopupMenuPane): Unit = {
-      ???
+  def addWeakReference(popupId: String, pane: PopupMenuPane): Unit = {
+    _panes.put(popupId, new WeakReference(pane))
   }
 
-  def showPopup(popupId : String, positionX : Double, positionY : Double, buttons : Future[Array[(String, Object)]], title : String ) : Unit = {
+  def showPopupThreadsafe(popupId: String, positionX: Double, positionY: Double, buttons: Future[Array[(String, Object)]], title: String, firstObjectType: Object): Unit = {
     val p = _panes.get(popupId)
     if (p != null) {
       val popuppane = p.get()
       if (popuppane != null) {
-        popuppane.showPopupThreadsafe(positionX, positionY, buttons, title)
+        popuppane.showPopupThreadsafe(positionX, positionY, buttons, title, firstObjectType)
       }
     }
   }
 
-  def setActiveHandler(popupId: String, handler : (Object, Object) => Unit) : Unit = {
+  def closePopupThreadsafe(popupId: String): Unit = {
+    val p = _panes.get(popupId)
+    if (p != null) {
+      val popuppane = p.get()
+      if (popuppane != null) {
+        popuppane.closeThreadsafe()
+      }
+    }
+  }
+
+  def setActiveHandler(popupId: String, handler: (Object, Object) => Unit): Unit = {
     val p = _panes.get(popupId)
     if (p != null) {
       val popuppane = p.get()
@@ -116,7 +236,7 @@ object PopupMenuPane{
   }
 
   //first object type
-  def setPassiveHandlers(popupId : String, handlers : Array[(String, Object => Unit)]) : Unit = {
+  def setPassiveHandlers(popupId: String, handlers: Array[(String, Object => Unit)]): Unit = {
     val p = _panes.get(popupId)
     if (p != null) {
       val popuppane = p.get()
